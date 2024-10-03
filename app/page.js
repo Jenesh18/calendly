2131dcf317e767e97c7aca39880b727e1f4bb9c9
Jenesh <jeneshrambo18@gmail.com>
2024-10-04 01:05:47 +0530

    day 2 neonDB setup and Hero section create

--- app/page.js
+++ app/page.js
@@ -1,8 +1,46 @@
+import HeroSection from "@/components/HeroSection";
+import {  Calendar, Clock, LinkIcon } from "lucide-react";
+
+
+const features = [
+  {
+    icon: Calendar,
+    title: "Create Events",
+    description: "Easily set up and customize your event types",
+  },
+  {
+    icon: Clock,
+    title: "Manage Availability",
+    description: "Define your availability to streamline scheduling",
+  },
+  {
+    icon: LinkIcon,
+    title: "Custom Links",
+    description: "Share your personalized scheduling link",
+  },
+];
+
+const howItWorks = [
+  { step: "Sign Up", description: "Create your free Schedulrr account" },
+  {
+    step: "Set Availability",
+    description: "Define when you're available for meetings",
+  },
+  {
+    step: "Share Your Link",
+    description: "Send your scheduling link to clients or colleagues",
+  },
+  {
+    step: "Get Booked",
+    description: "Receive confirmations for new appointments automatically",
+  },
+];
 
 export default function Home() {
   return (
-   <div>
-    hello world
-   </div>
+    <main className="container mx-auto px-4 py-16">
+      {/* HERO Section */}
+      <HeroSection />
+    </main>
   );
 }
